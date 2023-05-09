ee8c50a4710c8bc7c598c2f76a72d51a2a0cdd43
Andrew Winchell <awinchell@cobec.com>
2023-05-09 01:15:46 -0500

    Update jspdf.mjs

--- js/jspdf.mjs
+++ js/jspdf.mjs
@@ -1,16 +1,14 @@
-require([
-    "jspdf"
-], function(jsPDF) {
-    $("#refresh-iwl").on("click", () => {
-            const pdf = new jsPDF({
-                orientation: "portrait",
-                unit: "in",
-                format: [8.5, 11]
-            });
-            
-            pdf.text("Generate a PDF with JavaScript", 20, 20);
-            pdf.addPage();
-            pdf.text(20, 20, "The second page");
-            pdf.save("jsPDF_2pages.pdf");
-    });
-})
+import { jsPDF } from "/jspdf";
+
+$("#refresh-iwl").on("click", () => {
+        const pdf = new jsPDF({
+            orientation: "portrait",
+            unit: "in",
+            format: [8.5, 11]
+        });
+        
+        pdf.text("Generate a PDF with JavaScript", 20, 20);
+        pdf.addPage();
+        pdf.text(20, 20, "The second page");
+        pdf.save("jsPDF_2pages.pdf");
+});
