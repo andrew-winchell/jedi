1e5ff604fde2e652ba205a64877e132101c90b9f
Andrew Winchell <awinchell@cobec.com>
2023-05-23 22:27:59 -0500

    Update jspdf.js

--- js/jspdf.js
+++ js/jspdf.js
@@ -107,12 +107,6 @@ function pdfContent() {
             incident_id = $("#incident-id")[0].value;
             incident_name = $("#incident-name")[0].value;
             //pdf.text(incident_id + "\n" + incident_name, 20, 20);
-
-            //refreshText = $("#refresh-text")[0].textContent
-            //pdf.text(refreshText, 12, 12);
-            pdf.addPage();
-            headerFooter(pdf);
-            pdf.text(20, 20, "The second page");
             
             let base64string = pdf.output("bloburl");
             debugBase64(base64string);
