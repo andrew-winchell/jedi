1315614cc7b5b8c8b0346d111780110dbcdf8715
Andrew Winchell <awinchell@cobec.com>
2023-05-25 07:13:45 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -117,8 +117,9 @@ require([
                                 delay(500)
                                     .then(() => {
                                         $(ta_id).append(
+                                            "<calcite-button icon-start='handle-vertical' slot='action' scale='l' appearance='transparent' kind='neutral'></calcite-button>" +
                                             "<calcite-input placeholder='Enter takeaway' max-length='250' type='textarea' class='takeaways' value='" + takeaway.attributes.Key_Takeaways_other2 + 
-                                            "'><span class='handle'></span><calcite-button icon-start='trash' slot='action' scale='l' appearance='transparent' kind='neutral'></calcite-button></calcite-input>"
+                                            "'><calcite-button icon-start='trash' slot='action' scale='l' appearance='transparent' kind='neutral'></calcite-button></calcite-input>"
                                         );
                                     }
                                 )
