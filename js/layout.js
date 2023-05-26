ba8e82d6af8d0a77cea0176438ae9ed6936ea7cc
Andrew Winchell <awinchell@cobec.com>
2023-05-26 15:32:53 -0500

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -121,7 +121,7 @@ delay(1000).then(() => {
     $($("#th-overview")[0].shadowRoot.children[0].children[0].children[0]).css("height", "250px")
 })
 
-$(".form-container").css("height", "calc(100% - " + $("#tab-container").css("height") + ")")
+$(".form-container").css("height", "calc(100% - 78px)")
     
 function delay (time) {
     return new Promise(resolve => setTimeout(resolve, time));
